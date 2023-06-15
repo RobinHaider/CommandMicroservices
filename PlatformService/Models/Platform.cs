710efa2f285ac691e1924ff034d02aef00136758
Robin Haider <robinhaider69@gmail.com>
2023-06-15 18:19:36 +0600

    Add project files.

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
+
+public class Platform
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = default!;
+
+    [Required]
+    public string Publisher { get; set; } = default!;
+
+    [Required]
+    public string Cost { get; set; } = default!;
+}
