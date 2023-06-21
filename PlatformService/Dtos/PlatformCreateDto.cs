c3ebd4f347899b11233aeca7c821ab9a0906682e
Robin Haider <robinhaider69@gmail.com>
2023-06-21 16:11:30 +0600

    dtos

---
+++ PlatformService/Dtos/PlatformCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Dtos;
+
+public class PlatformCreateDto
+{
+    [Required]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public string Publisher { get; set; } = null!;
+
+    [Required]
+    public string Cost { get; set; } = null!;
+}
---
+++ PlatformService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PlatformService.Dtos;
+
+public class PlatformReadDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Publisher { get; set; } = null!;
+
+    public string Cost { get; set; } = null!;
+}
