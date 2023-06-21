a07bf6ae23dbd7e64b9dd1195919939de07a45c1
Robin Haider <robinhaider69@gmail.com>
2023-06-21 16:18:54 +0600

    mapping profiles

---
+++ PlatformService/Helpers/MappingProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace PlatformService.Helpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Models.Platform, Dtos.PlatformReadDto>();
+        CreateMap<Dtos.PlatformCreateDto, Models.Platform>();
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase("InMemoryDb");
