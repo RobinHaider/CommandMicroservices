5e94819e182cc6a1c00a003ef0553af36e39044a
Robin Haider <robinhaider69@gmail.com>
2023-06-24 12:23:51 +0600

    platform controller

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System.Runtime.CompilerServices;
+
+namespace PlatformService.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepository _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(IPlatformRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PlatformReadDto>>> GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms...");
+
+        var platformItems = await _repository.GetAllPlatforms();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public async Task<ActionResult<PlatformReadDto>> GetPlatformById(int id)
+    {
+        var platformItem = await _repository.GetPlatformById(id);
+
+        if (platformItem == null)
+            return NotFound();
+        
+        return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PlatformReadDto>> CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+
+        _repository.CreatePlatform(platformModel);
+        await _repository.SaveChangesAsync();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+    }
+
+}
