85d8209a91eb7048dd56cafed8899409ce7312b4
Robin Haider <robinhaider69@gmail.com>
2023-07-16 20:40:43 +0600

    platforms controller

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers;
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+}
